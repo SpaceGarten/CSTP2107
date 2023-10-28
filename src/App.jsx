392e7acd5ee7e7773e50a287174533755e60a6a6
SpaceGarten <matt_hu@live.com>
2023-10-28 11:08:14 -0700

    Updates with component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import App from './components/App';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Main() {
   return (
-    <>
-      <Router>
-      <Switch>
-        <Route path="/news" component={News} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
-    </>
-  )
+    <div className="App">
+        <QuickCard />
+    </div>
+  );
 }
 
-export default App
+export default Main;
